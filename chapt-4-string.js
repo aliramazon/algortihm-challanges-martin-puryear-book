b56d8f1298b781d4fff7fdf995a7ef9b632e505e
Safarali <mr.safarali@gmail.com>
2016-11-24 16:38:27 -0800

    Solved upto page # 53

--- chapt-4-string.js
+++ chapt-4-string.js
@@ -205,3 +205,151 @@ function isSoftPalindrome(str) {
     }
     return reversedStr === cleanStr;
 }
+
+// ==== Is Alphabetical Order =========//
+
+/* Is Vowels Ordered */
+function isVowelsOrdered(str) {
+    var vowels = "aeiou";
+    var vowelsPos = [];
+
+    for(var i = 0; i < str.length; i ++){
+        var pos = vowels.indexOf(str[i]);
+
+        if(pos !== -1){
+            vowelsPos.push(pos);
+        }
+    }
+    // return vowelsPos === vowelsPos.sort(); This could do the job between 223-228.
+    for(var i = 0; i < vowelsPos.length-1; i ++){
+        if(vowelsPos[i] > vowelsPos[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+/* Is Letters Alphabetical Ordered */
+
+function isLettersOrdered(str) {
+    var abc = "abcdefghijklmnopqrstuvwxyz";
+    str = str.toLowerCase();
+
+    for(var i = 0; i < str.length-1; i ++){
+        if(abc.indexOf(str[i]) > abc.indexOf(str[i+1])){
+            return false;
+        }
+    }
+    return true;
+}
+
+// First Letter goes to the end //
+
+function wierdGreeting(str) {
+    var veryFirstLetter = str[0];
+    var firstLetterRemoved = str.split(str[0]).join("");
+    var firstLetter = firstLetterRemoved[0];
+    firstLetterRemoved = firstLetterRemoved.split(firstLetter).join(firstLetter.toUpperCase());
+
+    return firstLetterRemoved + " to the " + veryFirstLetter;
+}
+
+// Common Suffix.
+// We need to helper function, one for getting the longestWord
+//another for checking if given suffix exists in each word of array.
+//Several ways to solve this exists, however, I wanted to split into helpers to make sure
+// it is understandable and nested loops avoided;
+
+function getCommonSuffix(words) {
+    var longestWord = getShortestWord(words);
+    var commonSuffix = null;
+    for(var i = longestWord.length-1; i >= 0; i --){
+        var suffix = longestWord.slice(i);
+        if(isLastSuffixSame(words, suffix)){
+            commonSuffix = suffix;
+        }
+        else {
+            return commonSuffix;
+        }
+    }
+    return commonSuffix;
+}
+function isLastSuffixSame(words, suffix) {
+
+    for(var i = 0; i < words.length; i ++){
+        var chars = words[i].split(suffix);
+        if(words[i].length === 0 || chars[chars.length-1] !== ''){
+            return false;
+        }
+    }
+    return true;
+}
+
+function getShortestWord(words) {
+    var shortest = words[0];
+
+    for(var i = 0; i < words.length; i ++){
+        if(shortest.length > words[i].length){
+            shortest = words[i];
+        }
+    }
+    return shortest;
+}
+
+
+// Book Index
+
+function bookIndex(pages) {
+     var indexes = [];
+
+     for(var i = 0; i < pages.length; i ++){
+         var endIdx = getEndIdx(pages, i);
+         if(i === endIdx){
+             var range = pages[endIdx];
+         }
+         else {
+             var range = pages[i] + "-" + pages[endIdx];
+         }
+         indexes.push(range);
+         i = endIdx;
+
+     }
+     return indexes.join(",");
+}
+
+function getEndIdx(pages, startIdx) {
+    var startEle = pages[startIdx];
+    var endIdx = startIdx;
+
+    for(var i = startIdx; i < pages.length; i ++){
+        if(startEle === pages[i +1] - 1){
+            endIdx = i + 1;
+            startEle = pages[endIdx];
+        }
+        else {
+            return endIdx;
+        }
+    }
+    return endIdx;
+}
+
+
+// Drop the Mike
+
+function dropTheMike(phrase) {
+    phrase = phrase.split(" ");
+    if(phrase.indexOf("Mike") !== -1){
+        return "stunned silence";
+    }
+    var newPhrase = [];
+
+    for(var i = 0; i < phrase.length; i ++){
+        var word = phrase[i];
+
+        if(word !== ""){
+            var capitalizedWord = word.slice(0, 1).toUpperCase() + word.slice(1);
+            newPhrase.push(capitalizedWord);
+        }
+    }
+    return newPhrase.join(" ");
+}
